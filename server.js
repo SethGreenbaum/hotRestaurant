3bbe21ad76e3f34d5bbfdef13c49856bc92636c7
Seth Greenbaum <sethmgreenbaum@gmail.com>
2020-02-27 19:34:08 -0700

    more server stuff

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+var tables = [{
+    table: "Table 1",
+    id: 0,
+    name: "Jeffrey",
+    email: "fake@email.com",
+    phone: "555-555-555"
+}];
+var waitlist = [];
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "home.html"));
 });
@@ -19,16 +28,40 @@ app.get("/reserve", (req, res) => {
     res.sendFile(path.join(__dirname, "reservation.html"));
 });
 
-app.get("/api/waitlist", (req, res) => {
-    
-});
-
 app.get("/api/tables", (req, res) => {
-    
+    return res.json(tables)    
+});
+app.get("/api/waitlist", (req, res) => {
+    return res.json(waitlist)
 });
 
+app.post("/api/waitlist", function(req, res) {
+
+    var newTable = req.body;
+
+    // newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
+  
+    console.log(newTable);
+  
+    waitlist.push(newTable);
+  
+    res.json(newTable);
+});
+
+app.post("/api/tables", function(req, res) {
+
+    var newTable = req.body;
+
+    // newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
+  
+    console.log(newTable);
+  
+    tables.push(newTable);
+  
+    res.json(newTable);
+});
 
 app.listen(PORT, () => {
     console.log("App listening on http://localhost:" + PORT);
-  });
+});
   
